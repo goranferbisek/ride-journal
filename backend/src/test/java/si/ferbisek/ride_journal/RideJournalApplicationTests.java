a78efab29460aca465c6f1b64306b60d64b2fc94
Goran Ferbišek <59411605+goranferbisek@users.noreply.github.com>
2026-08-10 20:32:53 +0200

    Setup a SpringBoot backend with dependencies and profiles (#36)

---
+++ backend/src/test/java/si/ferbisek/ride_journal/RideJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package si.ferbisek.ride_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
