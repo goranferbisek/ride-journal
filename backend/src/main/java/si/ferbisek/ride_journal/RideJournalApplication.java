4e8176f1dab9a00134a5abc9f670b2314c0e8299
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-10 19:10:17 +0200

    Add a new Spring Boot project generated with Spring Initializr

---
+++ backend/src/main/java/si/ferbisek/ride_journal/RideJournalApplication.java
@@ -0,0 +1,13 @@
+package si.ferbisek.ride_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideJournalApplication.class, args);
+	}
+
+}
