3b38a86b342b1c62075e8d1084290729f07a4f69
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-11 21:42:39 +0200

    Enable auditing to populate non nullable created_at field

--- backend/src/main/java/si/ferbisek/ride_journal/entity/BaseEntity.java
+++ backend/src/main/java/si/ferbisek/ride_journal/entity/BaseEntity.java
@@ -1,10 +1,15 @@
 package si.ferbisek.ride_journal.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseEntity {
 
