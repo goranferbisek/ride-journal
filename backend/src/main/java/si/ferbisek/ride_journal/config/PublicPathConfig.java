8c89081fb984627f3185b6573637a0c8cbd93c78
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-21 17:15:41 +0200

    Configure authentication filter method

---
+++ backend/src/main/java/si/ferbisek/ride_journal/config/PublicPathConfig.java
@@ -0,0 +1,18 @@
+package si.ferbisek.ride_journal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class PublicPathConfig {
+
+    @Bean
+    public List<String> publicPaths() {
+        return List.of(
+                "/api/v1/auth/**"
+        );
+    };
+
+}
--- backend/src/main/java/si/ferbisek/ride_journal/config/SecurityConfig.java
+++ backend/src/main/java/si/ferbisek/ride_journal/config/SecurityConfig.java
@@ -1,33 +1,42 @@
 package si.ferbisek.ride_journal.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import si.ferbisek.ride_journal.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final List<String> publicPaths;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers("/api/auth/**").permitAll());
-
-        /*http
+        http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(request -> {
+                            publicPaths.forEach(path -> request.requestMatchers(path).permitAll());
+                            request.anyRequest().authenticated();
+                        }
+
                 );
-         */
 
         return http.build();
     }
@@ -40,6 +49,17 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getUsername())
+                        .password(user.getPasswordHash())
+                        .authorities(Collections.emptyList())
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
     }
 
 }
