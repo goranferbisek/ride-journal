d684a3228147daab6e5e1769a1f129446cd55034
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-27 17:31:27 +0200

    Redirect protected path to login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,29 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {AuthProvider} from "./auth/AuthContext.tsx";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router";
+import ProtectedRoute from "./auth/ProtectedRoute.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import GaragePage from "./pages/GaragePage.tsx";
+
+
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App/>}>
+    <Route path="/login" element={<LoginPage/>}/>
+    {/* public paths */}
+    <Route element={<ProtectedRoute/>}>
+      {/* protected paths */}
+      <Route path="/garage" element={<GaragePage/>}/>
+    </Route>
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App/>
+      <RouterProvider router={appRouter}/>
     </AuthProvider>
   </StrictMode>,
 )
