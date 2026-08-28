e6b45dad7524fdfe6071b6af81268c81bad48517
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-28 20:28:13 +0200

    Create a login page with cleaned styling

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,14 +1,17 @@
 import {Button, Container, Stack, TextField} from "@mui/material";
+import {Form} from "react-router";
 
 
 function LoginForm() {
   return <Container maxWidth="sm">
-    Login
-    <Stack spacing={2}>
-      <TextField label="Username"/>
-      <TextField label="Password" type="password"/>
-      <Button variant="contained">Login</Button>
-    </Stack>
+    <Form action="" method="POST">
+      <Stack spacing={2}>
+        <h2>Login</h2>
+        <TextField label="Username"/>
+        <TextField label="Password" type="password"/>
+        <Button variant="contained">Login</Button>
+      </Stack>
+    </Form>
   </Container>
 }
 
