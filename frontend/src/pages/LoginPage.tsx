397bef232915f08e193e8de6bfb65daf0988bb99
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-08-28 11:39:20 +0200

    Put together a simple login form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,17 @@
+import {Button, Container, Stack, TextField} from "@mui/material";
 
+
+function LoginForm() {
+  return <Container maxWidth="sm">
+    Login
+    <Stack spacing={2}>
+      <TextField label="Username"/>
+      <TextField label="Password" type="password"/>
+      <Button variant="contained">Login</Button>
+    </Stack>
+  </Container>
+}
 
 export default function LoginPage() {
-  console.log("Login page here")
-  return <div>Login page</div>
+  return <LoginForm/>
 }
