cdb26e3ce4d5f517ef105217f2dd4666dca493ee
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-09-08 11:51:26 +0200

    Add error endpoint to public paths to handle errors

--- backend/src/main/java/si/ferbisek/ride_journal/config/PublicPathConfig.java
+++ backend/src/main/java/si/ferbisek/ride_journal/config/PublicPathConfig.java
@@ -11,7 +11,8 @@ public class PublicPathConfig {
     @Bean
     public List<String> publicPaths() {
         return List.of(
-                "/api/v1/auth/**"
+                "/api/v1/auth/**",
+                "/error"
         );
     }
 
