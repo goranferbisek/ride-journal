100e9d630b85cdd91d77bbacebb359d136afb436
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-09-23 17:31:49 +0200

    Change card design

--- frontend/src/components/VehicleCard.tsx
+++ frontend/src/components/VehicleCard.tsx
@@ -1,4 +1,4 @@
-import {Button, Chip, ListItem, Stack} from "@mui/material";
+import {Button, Chip, ListItem, Stack, Typography} from "@mui/material";
 import type {Vehicle} from "../types/vehicle.ts";
 import {Link} from "react-router";
 
@@ -9,17 +9,15 @@ interface VehicleCardProps {
 export default function VehicleCard({vehicle}: VehicleCardProps) {
   return (
     <ListItem sx={{border: 1, borderColor: "divider", borderRadius: 2, p: 2}}>
-      <Stack>
-        <Stack direction="row" sx={{justifyContent: "space-between"}}>
-          <Stack>
+      <Stack direction="row" spacing={2} sx={{width: "100%", justifyContent: "space-between", alignItems: "center"}}>
+        <Stack direction="row" spacing={1.5}>
+          <Typography variant="h6">
             {vehicle.brand} {vehicle.model} ({vehicle.year})
-          </Stack>
-          <Button component={Link} to="/garage/vehicle/edit" variant="contained">Edit</Button>
-        </Stack>
-        <Stack spacing={2}>
+          </Typography>
           <Chip label={vehicle.licensePlate}/>
           <Chip label={vehicle.vin}/>
         </Stack>
+        <Button component={Link} to="/garage/vehicle/edit" variant="contained">Edit</Button>
       </Stack>
     </ListItem>
   );
