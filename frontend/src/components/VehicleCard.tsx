2c62ab9e43b2f108402792618c2b78cfa054857b
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-09-25 10:36:01 +0200

    Fix vehicle card

--- frontend/src/components/VehicleCard.tsx
+++ frontend/src/components/VehicleCard.tsx
@@ -11,11 +11,12 @@ export default function VehicleCard({vehicle}: VehicleCardProps) {
     <ListItem sx={{border: 1, borderColor: "divider", borderRadius: 2, p: 2}}>
       <Stack direction="row" spacing={2} sx={{width: "100%", justifyContent: "space-between", alignItems: "center"}}>
         <Stack direction="row" spacing={1.5}>
+          <Chip label={vehicle.type}/>
           <Typography variant="h6">
             {vehicle.brand} {vehicle.model} ({vehicle.year})
           </Typography>
-          <Chip label={vehicle.licensePlate}/>
-          <Chip label={vehicle.vin}/>
+          {vehicle.licensePlate && <Chip label={vehicle.licensePlate}/>}
+          {vehicle.vin && <Chip label={vehicle.vin}/>}
         </Stack>
         <Button component={Link} to={`/garage/vehicle/${vehicle.id}/edit`} variant="contained">Edit</Button>
       </Stack>
